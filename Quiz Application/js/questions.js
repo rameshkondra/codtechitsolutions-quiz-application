93a9e98020924a0c4153eaea1102e2fe8fd8c84a
Ramesh <kondraramesh9999@gmail.com>
2024-01-21 19:51:26 +0530

    quiz app

---
+++ Quiz Application/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 3,
+  question: "What does JS stand for?",
+  answer: "JavaScript",
+  options: [
+    "JsonScript",
+    "JavaScript",
+    "JavaScripting",
+    "JavaStyle"
+  ]
+},
+  {
+  numb: 4,
+  question: "The “ var”  and “function” are known as ?",
+  answer: "Declaration statements",
+  options: [
+    "Declaration statements",
+    "Data types",
+    "keywords",
+    "Prototypes"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which of these is not a keyword?",
+  answer: " use strict",
+  options: [
+    "debugger",
+    "use strict",
+    "with",
+    "if"
+  ]
+},
+{
+  numb: 6,
+  question: "How can we change the background color of an element?",
+  answer: "background-color",
+  options: [
+    "background-color",
+    "color",
+    "Both A and B",
+    "None of the above"
+  ]
+},
+{
+  numb: 7,
+  question: "How can we change the text color of an element?",
+  answer: "color",
+  options: [
+    "background-color",
+    "color",
+    "Both A and B",
+    "None of the above"
+  ]
+},
+{
+  numb: 8,
+  question: "Which HTML tag is used to declare internal CSS?",
+  answer: "style",
+  options: [
+    "link",
+    "script",
+    "meta",
+    "style"
+  ]
+},
+{
+  numb: 9,
+  question: "How many sizes of headers are available in HTML by default?",
+  answer: "6",
+  options: [
+    "1",
+    "8",
+    "5",
+    "6"
+  ]
+},
+{
+  numb: 10,
+  question: "How to create an ordered list in HTML?",
+  answer: "ol",
+  options: [
+    "ul",
+    "href",
+    "ol",
+    "b"
+  ]
+},
+];
